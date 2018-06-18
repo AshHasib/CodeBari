c07bfa5931a3bbab27f4a5dadc456d7027e7c0a2
AshHasib <30811301+AshHasib@users.noreply.github.com>
2018-06-18 12:00:46 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+        window.sr=ScrollReveal();
+        sr.reveal('.navbar',{
+            duration:2000,
+            origin:'top'
+        });
+        sr.reveal('.jumbotron', {
+           duration:2000,
+            origin:'top'
+        });
+        sr.reveal('.jumbo-button', {
+           duration:2000,
+            delay:2000,
+            origin:'top'
+        });
+        sr.reveal('.middle-portion', {
+           duration:2000,
+            origin:'bottom'
+        });
+      
